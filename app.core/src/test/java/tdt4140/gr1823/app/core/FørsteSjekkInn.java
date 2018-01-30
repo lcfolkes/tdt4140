742618222756da32f82060188ce8fba4a3fdce57
ErlendHjelleStrandkleiv <ErlendHjelleStrandkleiv@dhcp-10-22-9-62.wlan.ntnu.no>
2018-01-30 14:24:27 +0100

    Created dummyTest in src/test/java subfolder as required

---
+++ app.core/src/test/java/tdt4140/gr1823/app/core/FørsteSjekkInn.java
@@ -0,0 +1,36 @@
+package tdt4140.gr1823.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FørsteSjekkInn {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		String name = "Gruppe-23";
+		
+		assertEquals("Gruppe-23", name);
+	}
+
+}
