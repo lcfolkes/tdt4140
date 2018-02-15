aeef4190d0f5ec1ac0b03fb3f39477c866dfe201
ErlendHjelleStrandkleiv <ErlendHjelleStrandkleiv@192.168.1.42>
2018-02-15 22:24:38 +0100

    Changed fileName to include lookup-word "Test". Include with all files.

--- app.core/src/test/java/tdt4140/gr1823/app/core/SjekkInnTest.java
+++ app.core/src/test/java/tdt4140/gr1823/app/core/SjekkInnTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class FørsteSjekkInn {
+public class SjekkInnTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
