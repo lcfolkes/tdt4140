1f27bdfbd2dc8b2e32f61f96b1ec3e321a97b898
Lars Folkestad <lcfolkes@stud.ntnu.no>
2018-02-26 18:31:23 +0100

    testing with scenebuilder

--- tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFiltering.java
+++ tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFiltering.java
@@ -5,20 +5,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 
 public class FxFiltering extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxFiltering.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
+	
+	Stage window;
+	Stage scene;
+	Button button; 
+	
+public static void main(String[] args) {
         launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    		window = primaryStage; 
+    		window.setTitle("FilterFunc");
+    		button = new Button("Click me!");
+        Parent root = FXMLLoader.load(getClass().getResource("FxFiltering.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+	
 }
--- tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
+++ tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
@@ -1,15 +1,35 @@
 package tdt4140.gr1823.app.ui;
 
-import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FxFilteringController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+
+public class FxFilteringController implements Initializable {
 	
 	public Button button;
+	public ToggleGroup toggleGroup;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("Loading user data...");
+		// TODO Auto-generated method stub
+		
+	}
 	
 	public void handleButtonClick() {
 		System.out.println("Run some code the user doesn't see");
-		button.setText("Stop touching me!");
+		button.setText("Never gonna give you up\n" + 
+				"Never gonna let you down\n" + 
+				"Never gonna run around and desert you\n" + 
+				"Never gonna make you cry\n" + 
+				"Never gonna say goodbye\n" + 
+				"Never gonna tell a lie and hurt you!");
 	}
+
+	
 	
 	
 }
