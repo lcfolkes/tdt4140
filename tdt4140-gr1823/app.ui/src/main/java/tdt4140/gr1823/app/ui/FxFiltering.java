aa12d89a417fa85a2ebaab50ffd2808b4f0156fa
Lars Folkestad <lcfolkes@stud.ntnu.no>
2018-02-27 20:05:28 +0100

    implemented choicebox interface for gender specification

--- tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFiltering.java
+++ tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFiltering.java
@@ -2,29 +2,52 @@ package tdt4140.gr1823.app.ui;
 
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.geometry.Insets;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.FlowPane;
+
 
 
 public class FxFiltering extends Application {
 	
 	Stage window;
 	Stage scene;
-	Button button; 
 	
 public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
+    		
     		window = primaryStage; 
     		window.setTitle("FilterFunc");
-    		button = new Button("Click me!");
-        Parent root = FXMLLoader.load(getClass().getResource("FxFiltering.fxml"));
+    		Parent root = FXMLLoader.load(getClass().getResource("FxFiltering.fxml"));
+    		
+    		//Separator separator = new Separator();
+    		//FlowPane root = new FlowPane(); 
+    		/*Label label = new Label("Select gender:");
+    		
+    		ObservableList<String> genders 
+            = FXCollections.observableArrayList("MALE", "FEMALE"); //get from Gender() enum/class
+ 
+    		ChoiceBox<String> choiceBox = new ChoiceBox<String>(FXCollections.observableArrayList(genders));
+    		*/
+    		/*root.setPadding(new Insets(10));
+    		 
+            root.getChildren().addAll(label, choiceBox);
+            root.setPadding(new Insets(10));
+            root.setHgap(10);
+    */
+        
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
--- tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
+++ tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
@@ -4,18 +4,34 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToggleGroup;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.*; 
 
 public class FxFilteringController implements Initializable {
 	
 	public Button button;
 	public ToggleGroup toggleGroup;
 	
+	ObservableList<String> genders 
+    = FXCollections.observableArrayList("MALE", "FEMALE"); //get from Gender() enum/class
+	
+	@FXML 
+	private ChoiceBox choiceBox; //references id:choiceBox in FXML
+	//choiceBox = new ChoiceBox<String>(FXCollections.observableArrayList(genders));
+	
+	@FXML
+	private Label gender; //references id:gender in FXML
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("Loading user data...");
-		// TODO Auto-generated method stub
+		gender.setText("Specify gender:"); //connects to id:gender in FXML
+		choiceBox.setItems(genders);
+		
 		
 	}
 	
@@ -28,6 +44,7 @@ public class FxFilteringController implements Initializable {
 				"Never gonna say goodbye\n" + 
 				"Never gonna tell a lie and hurt you!");
 	}
+	
 
 	
 	
