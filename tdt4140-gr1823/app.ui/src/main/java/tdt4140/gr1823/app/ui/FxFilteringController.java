7ed902c741eb616e54380e1b115e187a1d5a2b6c
Lars Folkestad <lcfolkes@stud.ntnu.no>
2018-03-02 17:09:25 +0100

    improved ui for filter function
    added textfield to specify age group. working on handling exceptions #14

--- tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
+++ tdt4140-gr1823/app.ui/src/main/java/tdt4140/gr1823/app/ui/FxFilteringController.java
@@ -2,7 +2,6 @@ package tdt4140.gr1823.app.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.Initializable;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -17,10 +16,10 @@ public class FxFilteringController implements Initializable {
 	
 	public ToggleGroup toggleGroup;
 	
-	ObservableList<String> genders = FXCollections.observableArrayList("MALE", "FEMALE"); 
+	ObservableList<String> genders = FXCollections.observableArrayList(null,"MALE", "FEMALE"); 
 	//get from Gender() enum/class, choices in choicebox for gender selection
 	
-	ObservableList<String> ages = FXCollections.observableArrayList("Under 25", "25-50", "Over 50");
+	ObservableList<String> ages = FXCollections.observableArrayList(null, "Under 25", "25-50", "Over 50");
 	//choices in choicebox for selecting age group
 	
 	@FXML
@@ -30,38 +29,64 @@ public class FxFilteringController implements Initializable {
 	private ChoiceBox<String> cbGender; //references id:cbGender in FXML
 	
 	@FXML
-	private ChoiceBox<String> cbAgeGroup; //references id: cbAgeGroup fxml
+	private TextField textInput1;
 	
-	@FXML
-	private Label gender; //references id:gender in FXML
-	@FXML
-	private Label ageGroup; //references id:ageGroup 
+	@FXML 
+	private TextField textInput2;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("Loading user data...");
 		
-		gender.setText("Specify gender:"); //connects to id:gender in FXML
+		//gender.setText("Specify gender:"); //connects to id:gender in FXML
 		cbGender.setItems(genders);
+		cbGender.setValue(null); //default value
 		
-		
-		ageGroup.setText("Specify age group:"); //connects to id:ageGroup in fxml
-		cbAgeGroup.setItems(ages); 
+		textInput1.setText(""); //default value 
+		textInput2.setText(""); //default value
 		
 		buttonDemographic.setText("Submit"); 
-		buttonDemographic.setOnAction(e -> getChoice(cbGender, cbAgeGroup)); 
+		buttonDemographic.setOnAction(e -> getChoice(cbGender, textInput1, textInput2));
 		//when pressing button getChoice gets called
 		
 	}
 	
+	//checking if age group input is valid. ie. not a string unless empty string and not invalid integer
+	private boolean isValidInput(TextField textInput) {
+		String input = textInput.getText();
+		int inputInt;
+		try {
+			inputInt = Integer.parseInt(input);
+		}
+		catch(NumberFormatException e) {
+			if(input.isEmpty()){
+				return true;
+			}
+			return false;
+		}
+		if (inputInt < 0 || inputInt > 120){
+			return false; 
+		};
+		return true;
+	}
+
 	//To get the values of the selected items. Both gender and age
 	//Need to implement method that returns enum Gender object? 
-	private void getChoice(ChoiceBox<String> choiceBox1, ChoiceBox<String> choiceBox2 ) {
-		String gender = choiceBox1.getValue();
-		String age = choiceBox2.getValue(); 
-		System.out.println(gender);
-		System.out.println(age);
+	private void getChoice(ChoiceBox<String> choiceBox1, TextField input1, TextField input2) {
+		if(!(isValidInput(input1) && isValidInput(input2))){
+			System.out.println("Invalid input. Please try again");//popup invalid input. try again
+		}
+		else {
+			String gender = choiceBox1.getValue();
+			String fromAge = input1.getText();
+			String toAge = input2.getText();
+			System.out.println(gender + fromAge + toAge);
+			//calculateSteps(gender, fromAge, toAge);
+		}
+		
+		
 	}
+	
 
 	
 
